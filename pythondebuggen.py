66f0ff8dc209219b822212d1381e726b55c0bca2
Anne2GIT <annemanders2@gmail.com>
2017-11-15 14:26:46 +0100

    wifi werkt weer, voortgang tot zover

--- pythondebuggen.py
+++ pythondebuggen.py
@@ -2,12 +2,6 @@
 # Datum: 4/5/6 (tel kwijtgeraakt)
 # Versie: 29-10-2017
 
-# Voel je vrij om de variabelen/functies andere namen te geven als je die logischer vind.
-
-# Opmerking: Het alpaca bestand is erg groot! Neem eerst een klein proefstukje van het bestand, met 5 tot 10 fasta's.
-# Ga je runnen met het echte bestand, geef je programma dan even de tijd.
-
-
 def main(): 
     try:
         bestand = open("Alpacaklein.fa") # Voer hier de bestandsnaam van het juiste bestand in, of hernoem je bestand
@@ -22,22 +16,39 @@ def main():
     headers, seqs = lees_inhoud(bestand) 
    
     zoekwoord = input("Geef een zoekterm op: ")
-    for element in headers:
-        if zoekwoord in element:
-            if zoekwoord != "":
+    if zoekwoord != "":
+        for element in headers:
+            if zoekwoord in element:
                 print(element)  
-    for element in seqs:      
-        if zoekwoord in element:
-            if zoekwoord != "":
+        for element in seqs:      
+            if zoekwoord in element:
                 print(element)
     print()
-            
-    count = -1
-    for element in seqs:
-        count += 1
-        print(headers[count])
-        print(is_dna(element))  # of in 1 keer
 
+    try:         
+        count = -1
+        truecount = 0
+        falselijst = []
+    
+        for element in seqs:
+            count += 1
+            
+            DNA = is_dna(element)
+            if DNA == True or DNA == "True":
+                truecount += 1
+            
+            if DNA == "False" or DNA == False:
+                falselijst.append(headers[count])
+                
+        if truecount == len(seqs):
+            print("All sequences are DNA.")
+        else:
+            print("Not all sequences are DNA. These are not recognised as DNA: ")
+            for element in falselijst:
+                print(element)
+    except IndexError:
+        print("The lists don't have the samen length. Check lees_inhoud() for faults.")
+        return
     knipt(seqs, headers)
     
 """
@@ -53,26 +64,50 @@ def lees_inhoud(bestands_naam):
     sequentie= ""
     headers= []
     seqs= []
-
+    #if ">" not in bestands_naam:
+        #print("The file in not in FASTA format. Make sure it is before starting the program again.")
+        #return
+    
+    #try:
+    #count = 0 
     for regel in bestands_naam:
 
         regel = regel.strip()
+        waar = is_dna(regel)
+        if waar =="False" or waar == False:
+            headers.append(regel)
         for i in regel:
-            if i == '>':               
+            
+            if i == ">":# or not all([i==i.upper]):
                 headers.append(regel)
+                               
                 if sequentie:
                     seqs.append("".join(sequentie)) ## blijkbaar sneller dan .append,
                     sequentie = ""                  ## handiger bij grote bestanden
                 break
             else:
                 regel = regel.upper()
-        
+            
         if all([k==k.upper() for k in regel]):
             sequentie = sequentie + regel
     if sequentie:
         seqs.append("".join(sequentie))
-
+    #print(count, len(seqs), len(headers)) # 1header minder is daardoor ook sequentie minder
+    #if count != len(seqs):
+        #print("File is not in fasta format. Make sure it is before you start the program again.")
+        #return
+    print(len(headers), len(seqs))
     return headers, seqs
+    #except count != len(seqs):
+        #print("File is not in fasta format. Make sure it is before you start the program again.")
+'''if regel.startswith(">"):
+            headers.append(regel)
+        
+        for i in regel:
+            if i in range(0,9):
+                if not regel.startswith(">"): 
+                    headers.append(regel)
+                    count += 1'''
 
 
 """
@@ -81,17 +116,18 @@ Indien ja, return True
 Zo niet, return False
 """
     
-def is_dna(seq):
+def is_dna(element):
     nuccount = 0
-    for element in seq:
-        for ch in element:
-            if ch in ["A", "T", "C", "G"]: ## wil dat het true print boven/onder sequentie 
-                nuccount += 1
-        if nuccount == len(element):  #betekent alle nucleotiden a, t, c of g zijn
-            return True 
-        else:
-            print(nuccount, len(element))
-            return False 
+    DNA = "True"
+    
+    for ch in element:
+        if ch in ["A", "T", "C", "G"]: 
+            nuccount += 1
+    if nuccount == len(element):  
+        return True
+    else:
+        #print(nuccount, len(element))
+        return False 
 
     
 """
@@ -103,26 +139,34 @@ Hiervoor mag je kiezen wat je returnt, of wellicht wil je alleen maar printjes m
 def knipt(seq, header):
     
     enzymen = open("enzymen.txt","r")
-    elementcount = -1
-    matchlijst = [] 
-    frgmntlijst = []
-    enzlijst = []
-    for element in seq:     
-        sublijstmatch = []  
-        elementcount += 1   
-        for regel in enzymen:                   #vanaf hier uit forloop element--> alleen laatste doornemen
-            regel = regel.replace("^", "")      #vanaf hier in de forloop element-->alleen eerste doornemen
-            enz, frgmnt = regel.split()
-            frgmntlijst.append(frgmnt)
-            enzlijst.append(enz)
+    try:
+        elementcount = -1
+        matchlijst = [] 
+        frgmntlijst = []
+        enzlijst = []
+        for element in seq:     
+            sublijstmatch = []  
+            elementcount += 1   
+            for regel in enzymen:                   
+                regel = regel.replace("^", "")      
+                enz, frgmnt = regel.split()
+                test = is_dna(frgmnt)
+                if test:
+                    frgmntlijst.append(frgmnt)
+                    enzlijst.append(enz)
+                else:
+                    print("The list of enzyms is faulty. Check it before starting the program again")
+                    return
 
-        for x in range(len(frgmntlijst)):
-            if frgmntlijst[x] in element:       
-                sublijstmatch.append(enzlijst[x]) 
-        matchlijst.append(sublijstmatch)
+            for x in range(len(frgmntlijst)):
+                if frgmntlijst[x] in element:       
+                    sublijstmatch.append(enzlijst[x]) 
+            matchlijst.append(sublijstmatch)
+            print(elementcount, len(header))
 
-        print(80*"-")
-        print("Matches restrictieenzymen", header[elementcount], ":", matchlijst[elementcount])                                                              #print(enz, frgmnt) # komma = met spatie, plus = zonder spatie
+            print(80*"-")
+            print("Matches restrictieenzymen", header[elementcount], ":", matchlijst[elementcount])                                                              #print(enz, frgmnt) # komma = met spatie, plus = zonder spatie
 
-
+    except IndexError:
+        print()
 main()
